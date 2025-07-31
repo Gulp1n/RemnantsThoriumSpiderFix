8c590d7d6e007a27207c43afc4def423452ef1d1
Gulp1n <abeldenouden@hotmail.com>
2025-07-31 14:28:40 +0200

    0.1

---
+++ Content/Override.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Terraria.ModLoader;
+
+namespace RemnantsThoriumSpiderFix.Content;
+
+public class Override : GlobalNPC
+{
+    public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
+    {
+    }
+}
